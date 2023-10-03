102aa1b33a47c03b88a00de460474e57387cf783
Alex Verine <alexverine@me.com>
2023-10-03 19:36:42 +0200

    Add Rejection

--- sampler.py
+++ sampler.py
@@ -21,6 +21,7 @@ import precision_recall_kyn_utils
 import precision_recall_simon_utils
 import utils
 import losses
+from utils_rejection import get_sampler_function, get_sampling_function, test_sampling
 
 
 
@@ -34,6 +35,7 @@ def run_sampler(config):
   print(experiment_name)
 
   config['experiment_name'] = experiment_name
+
   # Optionally, get the configuration from the state dict. This allows for
   # recovery of the config provided only a state dict and experiment name,
   # and can be convenient for writing less verbose sample shell scripts.
@@ -68,17 +70,20 @@ def run_sampler(config):
   experiment_name = (config['experiment_name'] if config['experiment_name']
                        else utils.name_from_config(config))
   logging.info('Experiment name is %s' % experiment_name)
-  
+  utils.prepare_root(config)
+
   G = model.Generator(**config).cuda()
-  utils.count_parameters(G)
-  
+  D = model.Discriminator(**config).to(device)
+
+  config['batch_size'] = config['batch_size_eval']
+  # loaders = utils.get_data_loaders(**config)
   # Load weights
   logging.info('Loading weights...')
   # Here is where we deal with the ema--load ema weights or load normal weights
-  utils.load_weights(G if not (config['use_ema']) else None, None, state_dict, 
+  utils.load_weights(G if not (config['use_ema']) else None, D, state_dict, 
                      config['weights_root'], experiment_name, config['load_weights'],
                      G if config['ema'] and config['use_ema'] else None,
-                     strict=False, load_optim=False)
+                     strict=True, load_optim=False)
   # Update batch size setting used for G
   G_batch_size = max(config['G_batch_size'], config['batch_size']) 
   z_, y_ = utils.prepare_z_y(G_batch_size, G.dim_z, config['n_classes'],
@@ -88,75 +93,53 @@ def run_sampler(config):
   if config['G_eval_mode']:
     # logging.info('Putting G in eval mode..')
     G.eval()
+    D.eval()
   else:
     logging.info('G is in %s mode...' % ('training' if G.training else 'eval'))
     
   #Sample function
-  sample = functools.partial(utils.sample, G=G, z_=z_, y_=y_, config=config)  
-  if config['accumulate_stats']:
-    logging.info('Accumulating standing stats across %d accumulations...' % config['num_standing_accumulations'])
-    utils.accumulate_standing_stats(G, z_, y_, config['n_classes'],
-                                    config['num_standing_accumulations'])
-    
-  
-  # Sample a number of images and save them to an NPZ, for use with TF-Inception
-  if config['sample_npz']:
-    # Lists to hold images and labels for images
-    x, y = [], []
-    logging.info('Sampling %d images and saving them to npz...' % config['sample_num_npz'])
-    for i in trange(int(np.ceil(config['sample_num_npz'] / float(G_batch_size)))):
-      with torch.no_grad():
-        images, labels = sample()
-      x += [np.uint8(255 * (images.cpu().numpy() + 1) / 2.)]
-      y += [labels.cpu().numpy()]
-    x = np.concatenate(x, 0)[:config['sample_num_npz']]
-    y = np.concatenate(y, 0)[:config['sample_num_npz']]    
-    logging.info('Images shape: %s, Labels shape: %s' % (x.shape, y.shape))
-    npz_filename = '%s/%s/samples.npz' % (config['samples_root'], experiment_name)
-    logging.info('Saving npz to %s...' % npz_filename)
-    np.savez(npz_filename, **{'x' : x, 'y' : y})
-  
-  # Prepare sample sheets
-  if config['sample_sheets']:
-    logging.info('Preparing conditional sample sheets...')
-    utils.sample_sheet(G, classes_per_sheet=utils.classes_per_sheet_dict[config['dataset']], 
-                         num_classes=config['n_classes'], 
-                         samples_per_class=10, parallel=config['parallel'],
-                         samples_root=config['samples_root'], 
-                         experiment_name=experiment_name,
-                         folder_number=config['sample_sheet_folder_num'],
-                         z_=z_,)
-  # Sample interp sheets
-  if config['sample_interps']:
-    logging.info('Preparing interp sheets...')
-    for fix_z, fix_y in zip([False, False, True], [False, True, False]):
-      utils.interp_sheet(G, num_per_sheet=16, num_midpoints=8,
-                         num_classes=config['n_classes'], 
-                         parallel=config['parallel'], 
-                         samples_root=config['samples_root'], 
-                         experiment_name=experiment_name,
-                         folder_number=config['sample_sheet_folder_num'], 
-                         sheet_number=0,
-                         fix_z=fix_z, fix_y=fix_y, device='cuda')
-  # Sample random sheet
-  if config['sample_random']:
-    batch_sample = 100
-    # logging.info('Preparing random sample sheet...')
-    # loaders = utils.get_data_loaders(**{**config, 'batch_size': config['batch_size'] ,
-    #                                   'start_itr': state_dict['itr']})
-    
-    images, labels = sample()    
-    images = images[:batch_sample]
-    name = 'random_samples'
-    if config['trunc']:
-      name += "trunc_"+str(config['z_var']).replace('.', '_')
-    elif config['z_var'] != 1.0:
-      name += str(config['z_var']).replace('.', '_')
+  # sample = functools.partial(utils.sample, G=G, z_=z_, y_=y_, config=config)  
 
-    torchvision.utils.save_image(images.float(),
-                                 '%s/%s/%s.jpg' % (config['samples_root'], experiment_name, name),
-                                 nrow=int(batch_sample**0.5),
-                                 normalize=True)
+  sample = functools.partial(utils.sample, G=G, z_=z_, y_=y_, config=config)  
+  Sampling, M = get_sampling_function(config, sample, D)
+  Sampling.eval()
+  # loaders = utils.get_data_loaders(**{**config, 'original':False})
+
+  #load datasets: loaders[0]=G, loaders[1]=Dataset 
+  sampler = get_sampler_function(config, sampling=Sampling, D=D, sample=sample)
+  sample = functools.partial(sampler, test=False)
+  print(f'Rate : {1/M:.3f}')
+
+
+  if config['sampling'] is not None:
+    if config['sampling'] == 'DRS':
+      name = config['sampling'] + '_' +  str(config['gamma_drs'])
+      config['sampling_params'] = config['gamma_drs']
+    else:
+      name = config['sampling'] + '_' +  str(config['budget']) 
+      config['sampling_params'] = config['budget']
+  else:
+    name = "NoRS"
+    config['sampling_params'] = 1
+
+  batch_sample = 100
+  # logging.info('Preparing random sample sheet...')
+  # loaders = utils.get_data_loaders(**{**config, 'batch_size': config['batch_size'] ,
+  #                                   'start_itr': state_dict['itr']})
+  utils.seed_rng(config['seed'])
+
+  images, labels = sample()    
+
+  images = images[:batch_sample]
+
+  new_images = torch.from_numpy(images.float().cpu().numpy())
+  torchvision.utils.save_image(new_images.float(),
+                                '%s/%s/%s.jpg' % (config['samples_root'], experiment_name, name),
+                                nrow=int(batch_sample**0.5),
+                                normalize=True)
+  
+  utils.seed_rng(config['seed'])
+  N, Na = test_sampling(functools.partial(sampler, test=True))
 
   # Get Inception Score and FID
   get_inception_metrics = inception_utils.prepare_inception_metrics(config['dataset'], config['parallel'], config['no_fid'])
@@ -165,14 +148,21 @@ def run_sampler(config):
   get_pr_curve = precision_recall_simon_utils.prepare_pr_curve(config)
   # Prepare a simple function get metrics that we use for trunc curves
   def get_metrics():
-    sample = functools.partial(utils.sample, G=G, z_=z_, y_=y_, config=config)    
-    # Psi, Rsi, Psa, Rsa = get_pr_curve(sample, "eval")
-    Psi, Rsi, Psa, Rsa = 0, 0, 0, 0 
+    utils.seed_rng(config['seed'])
+
+    Psi, Rsi, Psa, Rsa = get_pr_curve(sample, "eval")
+    # Psi, Rsi, Psa, Rsa = 0, 0, 0, 0 
+    utils.seed_rng(config['seed'])
+
     IS_mean, IS_std, FID = get_inception_metrics(sample, config['num_inception_images'], 
                                                  num_splits=10, 
                                                  prints=False,
                                                  use_torch=False)
+    # IS_mean, IS_std, FID = 0, 0, 0
+    utils.seed_rng(config['seed'])
+
     P, R, D, C = get_pr_metric(sample)
+    # P, R, D, C = 0, 0, 0, 0
     
     # Prepare output string
     outstring = 'Using %s weights ' % ('ema' if config['use_ema'] else 'non-ema')
@@ -183,6 +173,8 @@ def run_sampler(config):
       outstring += 'with batch size %d, ' % G_batch_size
     if config['accumulate_stats']:
       outstring += 'using %d standing stat accumulations, ' % config['num_standing_accumulations']
+    outstring += '\nItr %d: Estimated Acceptance Rate: %2.3f Vs Actual Acceptance Rate %2.3f' % (state_dict['itr'], 100/M, 100*Na/N)
+
     outstring += '\nItr %d: PYTORCH UNOFFICIAL Inception Score is %3.3f +/- %3.3f, PYTORCH UNOFFICIAL FID is %5.4f' % (state_dict['itr'], IS_mean, IS_std, FID)
     outstring += '\nItr %d: Kynkäänniemi Precision is %2.3f, Kynkäänniemi Recall is %2.3f' % (state_dict['itr'], P*100, R*100)
     outstring += '\nItr %d: Sajjadi Precision is %2.3f, Sajjadi Recall is %2.3f' % (state_dict['itr'], Psa*100, Rsa*100)
@@ -190,25 +182,29 @@ def run_sampler(config):
     outstring += '\nItr %d: Naeem Density is %2.3f, Naeem Coverage is %2.3f' % (state_dict['itr'], D, C)
 
     utils.write_evaldata(config['logs_root'], experiment_name, config, 
-                         {'itr': state_dict['itr'],'truc':config['trunc'], 'z_var':config['z_var'],  'IS_mean':IS_mean, 'IS_std' : IS_std, 'FID':FID, 'P':P, 'R':R, 'Psa':Psa, 'Rsa':Rsa,  'Psi':Psi, 'Rsi':Rsi,"D":D, "C":C })
+                         {'itr': state_dict['itr'], 'IS_mean':IS_mean, 'IS_std' : IS_std, 
+                          'FID':FID, 'P':P, 'R':R, 
+                          'Psa':Psa, 'Rsa':Rsa,  'Psi':Psi, 'Rsi':Rsi,
+                          "D":D, "C":C,  
+                          "sampling": config['sampling'],'params': config['sampling_params'] , 'rate':1/M})
 
     logging.info(outstring)
   if config['sample_inception_metrics']: 
     # logging.info('Calculating Inception metrics...')
     get_metrics()
     
-  # Sample truncation curve stuff. This is basically the same as the inception metrics code
-  if config['sample_trunc_curves']:
-    start, step, end = [float(item) for item in config['sample_trunc_curves'].split('_')]
-    logging.info('Getting truncation values for variance in range (%3.3f:%3.3f:%3.3f)...' % (start, step, end))
-    for var in np.arange(start, end + step, step):     
-      z_.var = var
-      # Optionally comment this out if you want to run with standing stats
-      # accumulated at one z variance setting
-      if config['accumulate_stats']:
-        utils.accumulate_standing_stats(G, z_, y_, config['n_classes'],
-                                    config['num_standing_accumulations'])
-      get_metrics()
+  # # Sample truncation curve stuff. This is basically the same as the inception metrics code
+  # if config['sample_trunc_curves']:
+  #   start, step, end = [float(item) for item in config['sample_trunc_curves'].split('_')]
+  #   logging.info('Getting truncation values for variance in range (%3.3f:%3.3f:%3.3f)...' % (start, step, end))
+  #   for var in np.arange(start, end + step, step):     
+  #     z_.var = var
+  #     # Optionally comment this out if you want to run with standing stats
+  #     # accumulated at one z variance setting
+  #     if config['accumulate_stats']:
+  #       utils.accumulate_standing_stats(G, z_, y_, config['n_classes'],
+  #                                   config['num_standing_accumulations'])
+  #     get_metrics()
 
 def main():
   # parse command line and run    
--- utils_rejection.py
+++ utils_rejection.py
@@ -22,24 +22,27 @@ class NoSampling(nn.Module):
   
     
 class DRSampling(nn.Module):
-  def __init__(self, params, M):
+  def __init__(self, params, M, Zq):
     super(DRSampling, self).__init__()
     self.gamma_drs = params["gamma_drs"]
     self.eps_drs = params["eps_drs"]
     self.M = M
+    self.Zq = Zq
 
   def forward(self, pq=None):
-    return torch.sigmoid(torch.log(pq) - torch.log(self.M) - torch.log(1-pq/self.M*np.exp(-self.eps_drs)) - self.gamma_drs)
+    return torch.sigmoid(torch.log(pq/self.Zq) - torch.log(self.M) - torch.log(1-pq/self.M*np.exp(-self.eps_drs)) - self.gamma_drs)
 
 
 
 
 class BudgetSampling(nn.Module):
-  def __init__(self, params, M):
+  def __init__(self, params, M, Zq):
     super(BudgetSampling, self).__init__()
     self.budget = params['budget']
     self.c = 1
     self.M = M
+    self.Zq = Zq
+
 
   # def compute_optimal_c(self, pq, n_iterations, depth=1, scale=1):
   #   if depth>30:
@@ -65,10 +68,11 @@ class BudgetSampling(nn.Module):
   #     if abs(torch.mean(torch.clamp(pqm*c, min=0, max=1))-self.budget)>abs(torch.mean(torch.clamp(pqm*c_deep, min=0, max=1))-self.budget):
   #       return c_deep
   #   return c
+
   def compute_optimal_c(self, pq, n_iterations, depth=1, scale=1):
-    pqm = pq/self.M
-    c_min = 1
-    c_max = 100000
+    pqm = torch.Tensor((pq/self.M).cpu().numpy()).cuda()
+    c_min = 1e-7
+    c_max = 1e10
     c_med = (c_min+c_max)/2
     n_iteration = 0
     while n_iteration<n_iterations:
@@ -85,7 +89,7 @@ class BudgetSampling(nn.Module):
     return torch.Tensor([c_med]).cuda()
 
   def update(self, pq):  
-    n_update = 1000 if self.training else 5000
+    n_update = 100 if self.training else 1000
   
     if self.budget is not None:
       c_opt = torch.clamp(self.compute_optimal_c(pq, n_update), min=1)
@@ -95,71 +99,77 @@ class BudgetSampling(nn.Module):
     self.c = c_opt
     
   def forward(self, pq, update=False):
+    pq = pq/self.Zq
     if update or self.training:
       self.update(pq)
     return torch.clamp(pq/self.M*self.c, min=0, max=1)
 
 
-def get_sampling_function(config, loader, D):
+def get_sampling_function(config, sample, D):
   if config['sampling'] is None:
-    return NoSampling().cuda(), None
+    return NoSampling().cuda(), 1
   else:
     rate = pq_fun(config)
     with torch.no_grad():
       M = torch.Tensor([0.]).cuda()
       pqs = torch.Tensor([0.]).cuda()
-
-      for i, (x,y) in enumerate(loader):
-          x, y = x.cuda(), y.cuda()
+      while pqs.shape[0]<1000:
+          x, y = sample()
           Dxf = D(x,y)
           pq = rate(Dxf)
           M = torch.max(torch.vstack((pq, M)))
           pqs = torch.vstack((pqs, pq))
+      Zq = torch.mean(pqs[1:])
+      M = M/Zq
+      # print(Zq)
+      M = torch.from_numpy(M .float().cpu().numpy()).cuda()
 
       if config['sampling'] == "DRS":
-        return DRSampling(config, M).cuda(), M
+        return DRSampling(config, M, Zq).cuda(), M
       else:
-        sampling = BudgetSampling(config, M).cuda()
+        sampling = BudgetSampling(config, M, Zq).cuda()
         sampling.eval()
         sampling(pqs[1: 1000], update=True)
         return sampling, M
         
 
 
-def get_sampler_function(config, sampling, D):
+def get_sampler_function(config, sampling, D, sample):
   rate = pq_fun(config)
   n_samples = config['batch_size']
   if config['sampling'] is None:
-    def RejectionSampler(loader, test=False):
+    def RejectionSampler(test=False):
       with torch.no_grad():
-        x_fake, y_fake = next(loader)
-        x_fake = x_fake.cuda()
+        x_fake, y_fake = sample()
         if test:
-          return x_fake, x_fake.shape[0], x_fake.shape[0]
+          return  x_fake.shape[0], x_fake.shape[0]
         else:
-          return x_fake, None
+          return x_fake, y_fake
     return RejectionSampler
 
-  def RejectionSampler(loader, test=False):
+  def RejectionSampler(test=False):
       with torch.no_grad():
         N = 0
         n_accepted = 0
         x_accepted = torch.Tensor().cuda()
+        y_accepted = torch.Tensor().cuda()
+
         while n_accepted < n_samples:
                 
-                x_fake, y_fake = next(loader)
-                x_fake = x_fake.cuda()
-                Dxf = D(x_fake, y_fake.cuda())
+                x_fake, y_fake = sample()
+                Dxf = D(x_fake, y_fake)
                 pq = rate(Dxf)
                 a = sampling(pq)
                 idx = a >= torch.rand((n_samples, 1)).cuda()
                 n_accepted += int(idx.sum())
                 x_accepted = torch.cat([x_accepted, x_fake[idx.view(-1)]], dim=0)
+                y_accepted = torch.cat([y_accepted, y_fake[idx.view(-1)]], dim=0)
+
                 N+=n_samples
         if test:
-          return x_accepted[:n_samples], N, n_accepted
+          return  N, n_accepted
         else:
-          return x_accepted[:n_samples], None
+          return x_accepted[:n_samples], y_accepted
 
   return RejectionSampler
 
@@ -167,7 +177,7 @@ def test_sampling(sampler):
   N = 0
   Na = 0
   while N<10000:
-    _, n, na = sampler()
+    n, na = sampler()
     N+=n
     Na+=na
   return N, Na
