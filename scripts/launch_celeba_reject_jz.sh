883797761a1878f9e6e2854c974da7805e70ee83
AlexVerine <alexverine@me.com>
2023-10-24 13:41:35 +0200

    scripts

--- scripts/launch_celeba_reject_jz.sh
+++ scripts/launch_celeba_reject_jz.sh
@@ -1,4 +1,33 @@
 # python launch_jeanzay.py --mode train \
+# --which_loss reject --which_div gan \ # --dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
+# --num_G_accumulations 1 --num_D_accumulations 1 \
+# --num_D_steps 1 --G_lr 1e-5 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
+# --G_attn 32 --D_attn 32 \
+# --G_nl relu --D_nl relu \
+# --SN_eps 1e-8 --BN_eps 1e-5 --adam_eps 1e-8 \
+# --G_ortho 0.0 \
+# --G_init xavier --D_init xavier \
+# --G_eval_mode \
+# --G_ch 32 --D_ch 32 \
+# --ema --use_ema --ema_start 2000 \
+# --test_every 10000 --save_every 15000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 500
+# #
+# python launch_jeanzay.py --mode train \
+# --which_loss reject --which_div gan \
+# --dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
+# --num_G_accumulations 1 --num_D_accumulations 1 \
+# --num_D_steps 1 --G_lr 5e-5 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
+# --G_attn 32 --D_attn 32 \
+# --G_nl relu --D_nl relu \
+# --SN_eps 1e-8 --BN_eps 1e-5 --adam_eps 1e-8 \
+# --G_ortho 0.0 \
+# --G_init xavier --D_init xavier \
+# --G_eval_mode \
+# --G_ch 32 --D_ch 32 \
+# --ema --use_ema --ema_start 2000 \
+# --test_every 10000 --save_every 15000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 500
+
+# python launch_jeanzay.py --mode train --TOBRS --update_every 500 \
 # --which_loss reject --which_div gan \
 # --dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
 # --num_G_accumulations 1 --num_D_accumulations 1 \
@@ -11,10 +40,10 @@
 # --G_eval_mode \
 # --G_ch 32 --D_ch 32 \
 # --ema --use_ema --ema_start 2000 \
-# --test_every 10000 --save_every 15000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 1000 
+# --test_every 10000 --save_every 15000 --num_best_copies 5 --num_save_copies 2 --seed 1   --num_epochs 1000 
 #
-# python launch_jeanzay.py --mode train \
-# --which_loss reject --which_div gan --TOBRS \
+# python launch_jeanzay.py --mode train --TOBRS  --update_every 1000 \
+# --which_loss reject --which_div gan \
 # --dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
 # --num_G_accumulations 1 --num_D_accumulations 1 \
 # --num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
@@ -26,8 +55,23 @@
 # --G_eval_mode \
 # --G_ch 32 --D_ch 32 \
 # --ema --use_ema --ema_start 2000 \
-# --test_every 10000 --save_every 15000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 1000 
+# --test_every 10000 --save_every 15000 --num_best_copies 5 --num_save_copies 2 --seed 1   --num_epochs 1000 
 
+# python launch_jeanzay.py --mode train \
+# --which_loss reject --which_div gan --TOBRS \
+# --dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
+# --num_G_accumulations 1 --num_D_accumulations 1 \
+# --num_D_steps 1 --G_lr 1e-4 --D_lr 1e-4 --D_B2 0.999 --G_B2 0.999 \
+# --G_attn 32 --D_attn 32 \
+# --G_nl relu --D_nl relu \
+# --SN_eps 1e-8 --BN_eps 1e-5 --adam_eps 1e-8 \
+# --G_ortho 0.0 \
+# --G_init xavier --D_init xavier \
+# --G_eval_mode \
+# --G_ch 32 --D_ch 32 \
+# --ema --use_ema --ema_start 2000 \
+# --test_every 10000 --save_every 15000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 1000 
+#
 # python launch_jeanzay.py --mode train \
 # --which_loss reject --which_div gan  \
 # --dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
@@ -58,41 +102,11 @@
 # --ema --use_ema --ema_start 2000 \
 # --test_every 5000 --save_every 5000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 200 --resume --name_suffix finetune_real
 
-python launch_jeanzay.py --mode train \
---which_loss reject --which_div gan  \
---dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
---num_G_accumulations 1 --num_D_accumulations 1 \
---num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
---G_attn 32 --D_attn 32 \
---G_nl relu --D_nl relu \
---SN_eps 1e-8 --BN_eps 1e-5 --adam_eps 1e-8 \
---G_ortho 0.0 \
---G_init xavier --D_init xavier \
---G_eval_mode \
---G_ch 32 --D_ch 32 \
---ema --use_ema --ema_start 2000 \
---test_every 5000 --save_every 5000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 200 --resume --name_suffix finetune_real
-
-python launch_jeanzay.py --mode train \
---which_loss reject --which_div gan  \
---dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
---num_G_accumulations 1 --num_D_accumulations 1 \
---num_D_steps 1 --G_lr 1e-6 --D_lr 4e-6 --D_B2 0.999 --G_B2 0.999 \
---G_attn 32 --D_attn 32 \
---G_nl relu --D_nl relu \
---SN_eps 1e-8 --BN_eps 1e-5 --adam_eps 1e-8 \
---G_ortho 0.0 \
---G_init xavier --D_init xavier \
---G_eval_mode \
---G_ch 32 --D_ch 32 \
---ema --use_ema --ema_start 2000 \
---test_every 5000 --save_every 5000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 200 --resume --name_suffix finetune_real
-
-# python3 launch_jeanzay.py --mode train \
-# --which_loss reject --which_div gan \
+# python launch_jeanzay.py --mode train \
+# --which_loss reject --which_div gan  \
 # --dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
 # --num_G_accumulations 1 --num_D_accumulations 1 \
-# --num_D_steps 1 --G_lr 1e-10 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
+# --num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
 # --G_attn 32 --D_attn 32 \
 # --G_nl relu --D_nl relu \
 # --SN_eps 1e-8 --BN_eps 1e-5 --adam_eps 1e-8 \
@@ -101,4 +115,34 @@ python launch_jeanzay.py --mode train \
 # --G_eval_mode \
 # --G_ch 32 --D_ch 32 \
 # --ema --use_ema --ema_start 2000 \
-# --test_every 5000 --save_every 5000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 150 --resume_no_optim --name_suffix Donly 
+# --test_every 5000 --save_every 5000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 200 --resume --name_suffix finetune_real
+#
+# python launch_jeanzay.py --mode train \
+# --which_loss reject --which_div gan  \
+# --dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
+# --num_G_accumulations 1 --num_D_accumulations 1 \
+# --num_D_steps 1 --G_lr 1e-6 --D_lr 4e-6 --D_B2 0.999 --G_B2 0.999 \
+# --G_attn 32 --D_attn 32 \
+# --G_nl relu --D_nl relu \
+# --SN_eps 1e-8 --BN_eps 1e-5 --adam_eps 1e-8 \
+# --G_ortho 0.0 \
+# --G_init xavier --D_init xavier \
+# --G_eval_mode \
+# --G_ch 32 --D_ch 32 \
+# --ema --use_ema --ema_start 2000 \
+# --test_every 5000 --save_every 5000 --num_best_copies 5 --num_save_copies 2 --seed 0   --num_epochs 200 --resume --name_suffix finetune_real
+
+python3 launch_jeanzay.py --mode train \
+--which_loss reject --which_div gan \
+--dataset CA64_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 128  \
+--num_G_accumulations 1 --num_D_accumulations 1 \
+--num_D_steps 1 --G_lr 1e-50 --D_lr 4e-5 --D_B2 0.999 --G_B2 0.999 \
+--G_attn 32 --D_attn 32 \
+--G_nl relu --D_nl relu \
+--SN_eps 1e-8 --BN_eps 1e-5 --adam_eps 1e-8 \
+--G_ortho 0.0 \
+--G_init xavier --D_init xavier \
+--G_eval_mode \
+--G_ch 32 --D_ch 32 \
+--ema --use_ema --ema_start 2000 \
+--test_every 2000 --save_every 5000 --num_best_copies 5 --num_save_copies 5 --seed 0   --num_epochs 150 --resume_no_optim --name_suffix Donly 
--- scripts/launch_cifar_reject_jz.sh
+++ scripts/launch_cifar_reject_jz.sh
@@ -1,42 +1,42 @@
-python launch_jeanzay.py --mode train \
---which_loss reject --which_div gan  --update_every 100 \
---shuffle --batch_size 50 --parallel \
---num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 800   \
---num_D_steps 4 --G_lr 2e-4 --D_lr 2e-5 \
---dataset C10_hdf5 \
---G_ortho 0.0 \
---G_attn 0 --D_attn 0 \
---G_init N02 --D_init N02 \
---ema --use_ema --ema_start 1000 \
---test_every 10000 --save_every 10000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
- --load_in_mem --num_workers 4  --G_eval_mode 
-
-python launch_jeanzay.py --mode train \
---which_loss reject --which_div gan  --update_every 10 \
---shuffle --batch_size 50 --parallel \
---num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 800   \
---num_D_steps 4 --G_lr 2e-4 --D_lr 2e-5 \
---dataset C10_hdf5 \
---G_ortho 0.0 \
---G_attn 0 --D_attn 0 \
---G_init N02 --D_init N02 \
---ema --use_ema --ema_start 1000 \
---test_every 10000 --save_every 10000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
- --load_in_mem --num_workers 4  --G_eval_mode 
-
-
-python launch_jeanzay.py --mode train \
---which_loss reject --which_div gan  --update_every 1 \
---shuffle --batch_size 50 --parallel \
---num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 800   \
---num_D_steps 4 --G_lr 2e-4 --D_lr 2e-5 \
---dataset C10_hdf5 \
---G_ortho 0.0 \
---G_attn 0 --D_attn 0 \
---G_init N02 --D_init N02 \
---ema --use_ema --ema_start 1000 \
---test_every 10000 --save_every 10000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
- --load_in_mem --num_workers 4  --G_eval_mode 
+# python launch_jeanzay.py --mode train --TOBRS  \
+# --which_loss reject --which_div gan  --update_every 100 \
+# --shuffle --batch_size 50 --parallel \
+# --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 800   \
+# --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-5 \
+# --dataset C10_hdf5 \
+# --G_ortho 0.0 \
+# --G_attn 0 --D_attn 0 \
+# --G_init N02 --D_init N02 \
+# --ema --use_ema --ema_start 1000 \
+# --test_every 10000 --save_every 10000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
+#  --load_in_mem --num_workers 4  --G_eval_mode 
+#
+# python launch_jeanzay.py --mode train --TOBRS \
+# --which_loss reject --which_div gan  --update_every 10 \
+# --shuffle --batch_size 50 --parallel \
+# --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 800   \
+# --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-5 \
+# --dataset C10_hdf5 \
+# --G_ortho 0.0 \
+# --G_attn 0 --D_attn 0 \
+# --G_init N02 --D_init N02 \
+# --ema --use_ema --ema_start 1000 \
+# --test_every 10000 --save_every 10000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
+#  --load_in_mem --num_workers 4  --G_eval_mode 
+#
+#
+# python launch_jeanzay.py --mode train --TOBRS \
+# --which_loss reject --which_div gan  --update_every 1 \
+# --shuffle --batch_size 50 --parallel \
+# --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 800   \
+# --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-5 \
+# --dataset C10_hdf5 \
+# --G_ortho 0.0 \
+# --G_attn 0 --D_attn 0 \
+# --G_init N02 --D_init N02 \
+# --ema --use_ema --ema_start 1000 \
+# --test_every 10000 --save_every 10000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
+#  --load_in_mem --num_workers 4  --G_eval_mode 
 
 # python launch_jeanzay.py --mode train \
 # --which_loss reject --which_div gan  --update_every 1000 --TOBRS \
@@ -132,15 +132,15 @@ python launch_jeanzay.py --mode train \
 
 
 
-# python launch_jeanzay.py --mode train \
-# --which_loss reject --which_div gan \
-# --shuffle --batch_size 50 --parallel \
-# --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 505   \
-# --num_D_steps 4 --G_lr 2e-10 --D_lr 2e-5 \
-# --dataset C10_hdf5 \
-# --G_ortho 0.0 \
-# --G_attn 0 --D_attn 0 \
-# --G_init N02 --D_init N02 \
-# --ema --use_ema --ema_start 1000 \
-# --test_every 100000 --save_every 5000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
-# --load_in_mem --num_workers 4  --G_eval_mode --name_suffix Donly --resume_no_optim
+python launch_jeanzay.py --mode train \
+--which_loss reject --which_div gan \
+--shuffle --batch_size 50 --parallel \
+--num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 510   \
+--num_D_steps 4 --G_lr 2e-50 --D_lr 1e-5 \
+--dataset C10_hdf5 \
+--G_ortho 0.0 \
+--G_attn 0 --D_attn 0 \
+--G_init N02 --D_init N02 \
+--ema --use_ema --ema_start 1000 \
+--test_every 10000 --save_every 5000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
+--load_in_mem --num_workers 4  --G_eval_mode --name_suffix Donly --resume_no_optim
